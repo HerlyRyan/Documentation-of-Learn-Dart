a9ff970e276ffb131039fd0aa59fd0231ebf43fd
Herly Riyanto Hidayat <herlyryanth@gmail.com>
2026-03-17 16:34:12 +0800

    learn: functions

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,6 +1,24 @@
 // import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
 import 'dart:io';
 
+// function that return double data type
+double converter(){
+  stdout.write('Masukkan suhu Fahrenheit: ');
+  double fahrenheit = double.parse(stdin.readLineSync()!);
+  var celcius = (fahrenheit - 32) * 5 / 9;
+  return celcius;
+}
+
+// function with parameter
+void greeting(String name, int bornYear) {
+  var age = DateTime.now().year - bornYear;
+  print('Hello $name, This year you are $age years old');
+}
+
+// funtion with null safety parameter and optional parameter
+void greetNewUser([String? name, int? age, bool isVerified = false]) {}
+
+// function main not return anything
 void main(List<String> arguments) {
   // Learn Variables
   // var nama = 'Herly Riyanto Hidayat';
@@ -21,9 +39,24 @@ void main(List<String> arguments) {
   // firstName = 7 // causing error
   
   // Receive user input
-  stdout.write('Name : ');
-  String name = stdin.readLineSync()!;
-  stdout.write('Age  : ');
-  int age = int.parse(stdin.readLineSync()!);
-  print('Hello $name, you are $age years old');
+  // stdout.write('Name : ');
+  // String name = stdin.readLineSync()!;
+  // stdout.write('Age  : ');
+  // int age = int.parse(stdin.readLineSync()!);
+  // print('Hello $name, you are $age years old');
+
+  // Learn try catch (exception)
+  // try {
+  //   var a = 7;
+  //   var b = 0;
+  //   print(a/b);
+  // } catch (e) {
+  //   print('Exception happened: $e');
+  // } finally {
+  //   print('Done task');
+  // }
+
+  // Learn functions
+  // print(converter());
+  // greeting('Herly Riyanto Hidayat', 2002);
 }
