687909908ce5d2a4f4ab09808def4a695fd4a159
Herly Riyanto Hidayat <herlyryanth@gmail.com>
2026-03-17 16:06:12 +0800

    learn: declaration of const and final variables

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -4,6 +4,12 @@ void main(List<String> arguments) {
   // Learn Variables
   var nama = 'Herly Riyanto Hidayat';
   var age = 23;
+
+  // Learn const and final
+  // both of them can not change after declaration
+  // const pi = 3.12;
+  // final match = 'Real Madrid vs Barcelona';
+
   print('Hello world: ${belajar_dart.calculate()}!');
   print('Hello $nama!. You are $age years old');
 }
