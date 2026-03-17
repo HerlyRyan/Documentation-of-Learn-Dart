f52edee8271ccf741982fbe0618438ddcec010d3
Herly Riyanto Hidayat <herlyryanth@gmail.com>
2026-03-17 16:18:56 +0800

    learn: data types

--- bin/belajar_dart.dart
+++ bin/belajar_dart.dart
@@ -1,15 +1,29 @@
-import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
+// import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
+import 'dart:io';
 
 void main(List<String> arguments) {
   // Learn Variables
-  var nama = 'Herly Riyanto Hidayat';
-  var age = 23;
+  // var nama = 'Herly Riyanto Hidayat';
+  // var age = 23;
+  // print('Hello world: ${belajar_dart.calculate()}!');
+  // print('Hello $nama!. You are $age years old');
 
   // Learn const and final
   // both of them can not change after declaration
   // const pi = 3.12;
   // final match = 'Real Madrid vs Barcelona';
 
-  print('Hello world: ${belajar_dart.calculate()}!');
-  print('Hello $nama!. You are $age years old');
+  // Learn data types
+  // var x; // dynamic data type (can change the value, ex: string to num)
+  // x = 'Hello'
+  // x = 7
+  // var firstName = 'Herly' // can change data type
+  // firstName = 7 // causing error
+  
+  // Receive user input
+  stdout.write('Name : ');
+  String name = stdin.readLineSync()!;
+  stdout.write('Age  : ');
+  int age = int.parse(stdin.readLineSync()!);
+  print('Hello $name, you are $age years old');
 }
