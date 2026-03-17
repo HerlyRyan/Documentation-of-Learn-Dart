0fcf24488e25a71661dd3830a61126b37607b533
Herly Riyanto Hidayat <herlyryanth@gmail.com>
2026-03-17 16:00:35 +0800

    learn: variables

---
+++ bin/belajar_dart.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
+
+void main(List<String> arguments) {
+  // Learn Variables
+  var nama = 'Herly Riyanto Hidayat';
+  var age = 23;
+  print('Hello world: ${belajar_dart.calculate()}!');
+  print('Hello $nama!. You are $age years old');
+}
